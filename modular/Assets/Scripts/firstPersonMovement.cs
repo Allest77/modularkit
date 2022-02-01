b8e4e46c7eeb2411ba3e16449df057d91b3595d8
Allest77 <30967524+Allest77@users.noreply.github.com>
2022-02-01 15:10:34 -0500

    Whelp, Movement Fixed
    I published this on the wrong Repository.
    
    - Movement works
    - Started shooting script

--- modular/Assets/Scripts/firstPersonMovement.cs
+++ modular/Assets/Scripts/firstPersonMovement.cs
@@ -6,8 +6,8 @@ public class firstPersonMovement : MonoBehaviour {
     //Publics 
     public CharacterController controller;
     public BoxCollider playerCollider;
-    public Transform cam;
-    public float speed = 32.0f, turnSmoothTime = 0.1f, turnSmoothVelocity, threshold = 56, jumpHeight = 30.2f, jumpForce = 20, gravity = -20.0f;
+    public Transform cam, target;
+    public float speed = 60.0f, turnSmoothTime = 0.1f, turnSmoothVelocity, threshold = 56, jumpHeight = 30.2f, jumpForce = 20, gravity = -20.0f;
     public bool isGrounded = true;
 
     //Privates
@@ -24,23 +24,31 @@ public class firstPersonMovement : MonoBehaviour {
         float vInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(hInput, 0.0f, vInput);
         rb.MovePosition(transform.position + movement * Time.deltaTime * speed * turnSmoothTime);
-
-
     }
 
     private void Update() {
         //Jump input.
-        if ((Input.GetButtonDown("Jump") && isGrounded))
-        {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
             isGrounded = false;
             GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.up) * jumpHeight * jumpForce);
             Debug.Log("Jumping");
         }
         else {
-            if ((Input.GetButtonUp("Jump") && !isGrounded)) {
+            if (Input.GetButtonUp("Jump") && !isGrounded) {
                 GetComponent<Rigidbody>().AddForce(transform.TransformDirection(Vector3.down) * gravity);
                 Debug.Log("HOLY AAAAAAAHHHHHHHHHHHHHH-");
             }
+        }
+
+        transform.LookAt(target);
+    }
+
+    void OnCollisionEnter (Collision hit) {
+        if (hit.gameObject.CompareTag("Wall")) {
+            isGrounded = false;
+        }
+        else {
+            isGrounded = true;
         }
     }
 }
